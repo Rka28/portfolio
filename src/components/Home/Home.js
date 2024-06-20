c434b307e0b15dd2fee9cf31e7d873823cee0599
rka28 <karim.bara@epitech.eu>
2024-06-20 11:14:06 +0200

    refraiche

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -26,7 +26,7 @@ import profile from '../asset/profil.png';
 import pro from '../asset/pro.jpg';
 import cinema from '../asset/cinema.png';
 import meetics from '../asset/meetics.png';
-// import quattreM from '../asset/4M.png';
+import quattreM from '../asset/4M.png';
 import Spotify from '../asset/Spotify.png';
 import LOL from '../asset/lol.jpg';
 import cv from '../asset/cv-Karim-Bara.pdf';
@@ -183,7 +183,7 @@ function Home() {
                 </div>
             </div>
 
-            {/* <div className="portfolio">
+            <div className="portfolio">
                 <img className="portfolio-logo" src={quattreM} alt="portfolio-logo" />
                 <div className="text-pro">
                     <h6>4M</h6>
@@ -194,7 +194,7 @@ function Home() {
                         <a href="https://main--4-wm.netlify.app/"><h4><CiShare1 size={40} />Live Demo</h4></a>
                     </div>
                 </div>
-            </div> */}
+            </div>
 
             <div className="projet">
                 <section id="Contact">
