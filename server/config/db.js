c001094217f34a68d64925feab5a51b6f9daf203
rka28 <karim.bara@epitech.eu>
2025-10-09 11:01:28 +0200

    Initial commit backend

--- server/config/db.js
+++ server/config/db.js
@@ -2,14 +2,13 @@ const mysql = require('mysql2/promise');
 require('dotenv').config();
 
 const pool = mysql.createPool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-  waitForConnections: true,
-  connectionLimit: 10,
-  queueLimit: 0
-});
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT || 3306,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    connectTimeout: 60000 // Augmenter le timeout à 60 secondes
+  });
 
 const testConnection = async () => {
   try {
